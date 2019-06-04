0c53c0a324392ad66fc84580b035b5dec036e770
Noor Octavian Anwar <alv1n@live.com>
2019-06-04 23:14:34 +0700

    Refactoring UserHandler resolvers

--- src/resolvers/handlers/UserHandler.js
+++ src/resolvers/handlers/UserHandler.js
@@ -5,17 +5,11 @@ module.exports = {
         return await User.findAll().then(data => data);
     },
     getById: async function (parent, args, context, info) {
-        const user = await User
-            .findAll({
-                where: {
-                    id: args.id
-                }
-            })
-            .then(data => data);
+        const user = await User.findOne({where: {id: args.id}}).then(data => data);
 
-        if (user.length < 1)
+        if (!user)
             throw new Error('User with passed ID was not found');
         else
-            return user[0].dataValues;
+            return user;
     }
 };
