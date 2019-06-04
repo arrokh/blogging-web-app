b0f415de1b9215f1929b821fa8915c082b148240
Noor Octavian Anwar <alv1n@live.com>
2019-06-05 02:49:07 +0700

    Refactory code after add ReactJS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require("path");
 const {ApolloServer} = require('apollo-server-express');
 
 const sequelize = require('./sequelize/initialize');
@@ -14,10 +15,21 @@ const server = new ApolloServer({
 });
 
 const app = express();
-const PORT = process.env.port|| 5000;
 
-server.applyMiddleware({app, path: '/graphql'});
+server.applyMiddleware({
+    app,
+    path: '/api'
+});
 
+app.get('/', (req, res) => {
+    res.send(`
+        Welcome to Blogging Web App API portal! 😀
+        <br>
+        <a href="http://localhost:3000/">Click this link to the main porta!</a>
+    `)
+});
+
+const PORT = process.env.port || 5000;
 app.listen({port: process.env.port}, () => {
     console.log(`🚀 Server started on port ${PORT}`);
 
